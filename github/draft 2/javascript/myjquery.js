1a9a24414756af2838ed311e3877d7fb2b053bb3
Alexandros Kourtis <akourtis@hauniv.edu>
2017-12-04 01:07:12 +0200

    Add files via upload

---
+++ github/draft 2/javascript/myjquery.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	/**$("#imgId").
+	mouseover(function() {
+	$( "#btn1" ).removeClass();
+  	$( "#btn1" ).addClass( "btnvisible");
+})
+	.mouseout(function() {
+   $( "#btn1" ).removeClass();
+  	$( "#btn1" ).addClass( "btnsnotvisible");
+  });
+  **/
+});
